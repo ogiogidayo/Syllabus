9641a5c3473158f060f18e20299bcf8f46486507
ogiogidayo <eiji.ogi0326@icloud.com>
2023-09-29 18:26:21 +0900

    schemasのエラー解消

--- main.py
+++ main.py
@@ -29,9 +29,9 @@ async def read_lectures(skip: int = 0, limit = 100, db: Session = Depends(get_db
 
 
 @app.post("/users", response_model=schemas.User)
-async def create_user(user: schemas.User, db: Session = Depends(get_db)):
+async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return crud.create_user(db=db,user=user)
 
 @app.post("/create_lecture", response_model=schemas.Lecture)
-async def create_lecture(lecture: schemas.Lecture, db: Session = Depends(get_db)):
+async def create_lecture(lecture: schemas.LectureCreate, db: Session = Depends(get_db)):
     return crud.create_lecture(db=db,lecture=lecture)
