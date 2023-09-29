d95b18d85aa3e90c482059f31763de0b6eb2d7fb
ogiogidayo <eiji.ogi0326@icloud.com>
2023-09-29 21:13:33 +0900

    schemasの継承ミス修正

--- viewer.py
+++ viewer.py
@@ -4,7 +4,7 @@ import requests
 import json
 import pandas as pd
 
-page = st.sidebar.selectbox('Choose your page', ['users', 'rooms', 'bookings'])
+page = st.sidebar.selectbox('Choose your page', ['users', 'シラバス登録', 'bookings'])
 
 if page == 'users':
     st.title('ユーザー登録画面')
@@ -24,151 +24,61 @@ if page == 'users':
         )
         if res.status_code == 200:
             st.success('ユーザー登録完了')
-        st.json(res.json())
+            st.json(res.json())
 
-elif page == 'rooms':
-    st.title('会議室登録画面')
 
-    with st.form(key='room'):
-        # room_id: int = random.randint(0, 10)
-        room_name: str = st.text_input('会議室名', max_chars=12)
-        capacity: int = st.number_input('定員', step=1)
-        data = {
-            # 'room_id': room_id,
-            'room_name': room_name,
-            'capacity': capacity
-        }
-        submit_button = st.form_submit_button(label='会議室登録')
+elif page == 'シラバス':
+    st.title('シラバス確認')
+    # シラバス一覧の取得
+    url_lectures = 'https://api-reserve-6grf.onrender.com/read_lecture'
+    res = requests.get(url_lectures)
+    lectures = res.json()
+    # lecture_name = {}
+    # for lecture in lectures:
+    #     lecture_name[lecture['lecture_name']] = {
+    #         'lecture_tech': lecture['lecture_tech'],
+    #         'lecture_info': lecture['lecture_info']
+    #     }
+    st.write('### 会議室一覧')
+    df_rooms = pd.DataFrame(lectures)
+    #df_rooms.columns = ['会議室名', '定員', '会議室ID']
+    st.table(df_rooms)
+
+elif page == 'シラバス登録':
+    st.title('シラバス登録画面')
+    # ユーザー一覧取得
+    url_users = 'https://syllabus-api-i5xd.onrender.com/users'
+    res = requests.get(url_users)
+    users = res.json()
+    users_dict = {}
+    for user in users:
+        users_dict[user['username']] = user['user_id']
+
+    with st.form(key='syllabus'):
+        username: str = st.text_input('ユーザーid')
+        lecture_name: str = st.text_input('講義名', max_chars=30)
+        lecture_tech: str = st.text_input('教員名', max_chars=20)
+        lecture_info: str = st.text_area('講義情報', max_chars=200)
+        submit_button = st.form_submit_button(label='シラバス登録')
 
     if submit_button:
-        url = 'https://api-reserve-6grf.onrender.com/rooms'
+        user_id: int =users_dict[username]
+
+        data = {
+            'lecture_name': lecture_name,
+            'lecture_tech': lecture_tech,
+            'lecture_info': lecture_info,
+            'user_id': user_id
+        }
+        # 会議室予約
+        url = 'https://syllabus-api-i5xd.onrender.com/create_lecture'
         res = requests.post(
             url,
             data=json.dumps(data)
         )
         if res.status_code == 200:
             st.success('会議室登録完了')
-        st.json(res.json())
-
-elif page == 'bookings':
-    st.title('会議室予約画面')
-    # ユーザー一覧取得
-    url_users = 'https://api-reserve-6grf.onrender.com/users'
-    res = requests.get(url_users)
-    users = res.json()
-    # ユーザー名をキー、ユーザーIDをバリュー
-    users_name = {}
-    for user in users:
-        users_name[user['username']] = user['user_id']
-
-    # 会議室一覧の取得
-    url_rooms = 'https://api-reserve-6grf.onrender.com/rooms'
-    res = requests.get(url_rooms)
-    rooms = res.json()
-    rooms_name = {}
-    for room in rooms:
-        rooms_name[room['room_name']] = {
-            'room_id': room['room_id'],
-            'capacity': room['capacity']
-        }
-
-    st.write('### 会議室一覧')
-    df_rooms = pd.DataFrame(rooms)
-    df_rooms.columns = ['会議室名', '定員', '会議室ID']
-    st.table(df_rooms)
-
-    url_bookings = 'https://api-reserve-6grf.onrender.com/bookings'
-    res = requests.get(url_bookings)
-    bookings = res.json()
-    df_bookings = pd.DataFrame(bookings)
-
-    users_id = {}
-    for user in users:
-        users_id[user['user_id']] = user['username']
-
-    rooms_id = {}
-    for room in rooms:
-        rooms_id[room['room_id']] = {
-            'room_name': room['room_name'],
-            'capacity': room['capacity'],
-        }
-
-    # IDを各値に変更
-    to_username = lambda x: users_id[x]
-    to_room_name = lambda x: rooms_id[x]['room_name']
-    to_datetime = lambda x: datetime.datetime.fromisoformat(x).strftime('%Y/%m/%d %H:%M')
-
-    # 特定の列に適用
-    df_bookings['user_id'] = df_bookings['user_id'].map(to_username)
-    df_bookings['room_id'] = df_bookings['room_id'].map(to_room_name)
-    df_bookings['start_datetime'] = df_bookings['start_datetime'].map(to_datetime)
-    df_bookings['end_datetime'] = df_bookings['end_datetime'].map(to_datetime)
-
-    df_bookings = df_bookings.rename(columns={
-        'user_id': '予約者名',
-        'room_id': '会議室名',
-        'booked_num': '予約人数',
-        'start_datetime': '開始時刻',
-        'end_datetime': '終了時刻',
-        'booking_id': '予約番号'
-    })
-    st.write('### 予約一覧')
-    st.table(df_bookings)
-
-
-    with st.form(key='booking'):
-        username: str = st.selectbox('予約者名', users_name.keys())
-        room_name: str = st.selectbox('会議室名', rooms_name.keys())
-        booked_num: int = st.number_input('予約人数', step=1, min_value=1)
-        date = st.date_input('日付: ', min_value=datetime.date.today())
-        start_time = st.time_input('開始時刻: ', value=datetime.time(hour=9, minute=0))
-        end_time = st.time_input('終了時刻: ', value=datetime.time(hour=20, minute=0))
-        submit_button = st.form_submit_button(label='予約登録')
-
-    if submit_button:
-        user_id: int = users_name[username]
-        room_id: int = rooms_name[room_name]['room_id']
-        capacity: int = rooms_name[room_name]['capacity']
-
-        data = {
-            'user_id': user_id,
-            'room_id': room_id,
-            'booked_num': booked_num,
-            'start_datetime': datetime.datetime(
-                year=date.year,
-                month=date.month,
-                day=date.day,
-                hour=start_time.hour,
-                minute=start_time.minute
-            ).isoformat(),
-            'end_datetime': datetime.datetime(
-                year=date.year,
-                month=date.month,
-                day=date.day,
-                hour=end_time.hour,
-                minute=end_time.minute
-            ).isoformat()
-        }
-        # 定員より多い予約人数の場合
-        if booked_num > capacity:
-            st.error(f'{room_name}の定員は、{capacity}名です。{capacity}名以下の予約人数のみ受け付けております。')
-        # 開始時刻 >= 終了時刻
-        elif start_time >= end_time:
-            st.error('開始時刻が終了時刻を越えています')
-        elif start_time < datetime.time(hour=9, minute=0, second=0) or end_time > datetime.time(hour=20, minute=0, second=0):
-            st.error('利用時間は9:00~20:00になります。')
+            st.json(res.json())
         else:
-            # 会議室予約
-            url = 'https://api-reserve-6grf.onrender.com/bookings'
-            res = requests.post(
-                url,
-                data=json.dumps(data)
-            )
-            if res.status_code == 200:
-                st.success('予約完了しました')
-            elif res.status_code == 404 and res.json()['detail'] == 'Already booked' :
-                st.error('指定の時間にはすでに予約が入っています。')
-
-
-
-
+            st.error(f'エラーステータスコード: {res.status_code}')
+            st.text(res.text)  # エラーレスポンス全体を表示
